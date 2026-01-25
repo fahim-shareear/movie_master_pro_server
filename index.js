215d1806d7d504b163f285925f61e191262e6041
Fahim <fahimshareear@gmail.com>
2026-01-25 22:53:35 +0600

    adding firebase-admin auth sdk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const admin = require('firebase-admin');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +7,11 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+const serviceAccount = require("./movie_master_pro_firebase_sdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.get('/', (req, res)=>{
     res.send("Server is up and running");
