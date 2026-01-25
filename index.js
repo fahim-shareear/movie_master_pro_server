0341e743d935d5d079e7e0ee2cb445fdce7c2763
Fahim <fahimshareear@gmail.com>
2026-01-26 01:03:48 +0600

    connecting to the MongoDB server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,10 +14,42 @@ const serviceAccount = require("./movie_master_pro_firebase_sdk.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+//mongodb connection link:
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@learning-server.eft4uy8.mongodb.net/?appName=learning-server`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 app.get('/', (req, res)=>{
     res.send("Server is up and running");
 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const movieCollection = client.db("movieMasterDB").collection("movies");
+        const watchListCollection = client.db("movieMasterDB").collection("watchList");
+
+
+        await client.db("admin").command({ping: 1});
+        console.log("Pinged your deployment. You have successfully connected to MongoDB!")
+    }
+    finally{
+
+    }
+};
+
+run().catch(console.dir);
+
+
 
 
 app.listen(port, ()=>{
