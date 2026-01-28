a4ab88868a9653a6bd860e5d77ac5907bee1e21c
Fahim <fahimshareear@gmail.com>
2026-01-28 16:41:26 +0600

    adding the live link in the cors for the front end

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 // 1. Full CORS Middleware (Now including PATCH)
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://movie-master-pro-82ff4.web.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
